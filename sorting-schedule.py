e33346f8237a977282f53bb1649538324e5ca5f3
artaly <70465647+artaly@users.noreply.github.com>
2021-01-19 17:36:02 +0800

    Added notes

--- sorting-schedule.py
+++ sorting-schedule.py
@@ -1,35 +1,32 @@
-def insertion_sort(array):
-    for i in range(1, len(array)):
-        key_item = array[i]
-        j = i - 1
-        while j >= 0 and array[j] > key_item:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key_item
-    return array
-
-
 menu_choice = input("""Menu: 
 1. View Sched
 2. Add Sched 
 
 Answer: """)
 
-if menu_choice == '2':
+# View Sched
+if menu_choice == '1':
+    print('')
+
+# Add Sched
+elif menu_choice == '2':
     sched_q = input('How many subjects will you add? ')
 
-    # this line should run n times (depending sa input sa sched_q)
+    # This line should run n times (depending sa input of sched_q) or bahala ka
+    # if you thought of something else 
     subject_time = input('Enter time: ')
     subject_name = input('Enter subject name: ')
 
     print('\nSubject added!\n', subject_name, '—', subject_time)
 
+    # Sort or Main Menu
     add_choice = input("""Select:
     1 - Sort
     2 - Return to Menu
     
     Answer: """)
 
+    # Sorting Methods
     if add_choice == '1':
         sorting_choice = input("""Choose a sorting method: 
         A. Bubble Sort
